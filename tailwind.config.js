b0c7904736ea732b7bc5cc21e2097f49a86460e3
faman272 <fathurrahmansmkt@gmail.com>
2025-03-21 10:22:05 +0700

    tambah files tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                'accent': '#46A358'
+            },
+            fontFamily: {
+                'poppins': ['Poppins', 'sans-serif']
+            }
+        },
+    },
+    plugins: [],
+}
